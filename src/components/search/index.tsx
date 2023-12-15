cf27b4ec74bc6547ac1db85f702503c26e831207
Harshit Shukla <harshitcr7777@gmail.com>
2023-12-16 03:29:53 +0530

    Created search component

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import IconButton from "@mui/material/IconButton";
+import InputBase from "@mui/material/InputBase";
+import SearchIcon from "@mui/icons-material/Search";
 
 const index = () => {
-  return <div>search</div>;
+  return (
+    <div>
+      <InputBase
+        sx={{ ml: 1, flex: 1 }}
+        placeholder="Search Google Maps"
+        inputProps={{ "aria-label": "search google maps" }}
+      />
+      <IconButton type="button" sx={{ p: "10px" }} aria-label="search">
+        <SearchIcon />
+      </IconButton>
+    </div>
+  );
 };
 
 export default index;
